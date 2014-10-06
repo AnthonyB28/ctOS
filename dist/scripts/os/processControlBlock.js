060d18b479734b0bd57103e8cfc9b8bd90e0957b
Anthony Barranco <anthony@lssclan.org>
2014-10-06 16:17:02 -0400

    PCB dist
    Apparently github has a really hard time ignoring dist folder now that
    I've been commiting to them since the beginning. I really really really
    hate this folder layout and thus hate git for ignoring my request.

---
+++ dist/scripts/os/processControlBlock.js
@@ -0,0 +1,24 @@
+﻿/* ------------
+Process Control Block
+Part of the OS kernel to manage a process.
+------------ */
+var CTOS;
+(function (CTOS) {
+    var ProcessControlBlock = (function () {
+        function ProcessControlBlock() {
+            // 0 = new ; 1 = running ; 2 = waiting ; 3 = ready; 4 = terminated
+            this.m_State = 0;
+            this.m_PID = 0;
+            this.m_Counter = 0;
+            this.m_Accumulator = 0;
+            this.m_X = 0;
+            this.m_Y = 0;
+            this.m_Z = 0;
+            this.m_MemBase = 0;
+            this.m_MemLimit = 0;
+        }
+        return ProcessControlBlock;
+    })();
+    CTOS.ProcessControlBlock = ProcessControlBlock;
+})(CTOS || (CTOS = {}));
+//# sourceMappingURL=processControlBlock.js.map
