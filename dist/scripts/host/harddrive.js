8b15adc09abe5621c5c751115a97093215c8223f
Anthony Barranco <anthony@lssclan.org>
2014-11-23 20:07:47 -0500

    Basis of hard drive hardware

--- dist/scripts/host/harddrive.js
+++ dist/scripts/host/harddrive.js
@@ -2,6 +2,11 @@
 Hard Drive
 Uses HTML5 local storage to access and modify data using Track-Sector-Block.
 The device driver will handle conversions to and from TSB, similar to memoryManager.
+001-077 = dir data
+100-377 = data
+0/1 - @@@ - filename or data
+in use bit - next TSB or @ invalid - data string from 4 bytes to 63
+Need 5 TSBs for a single swap file, aprox 15 available from TSB 302
 **/
 var CTOS;
 (function (CTOS) {
@@ -17,6 +22,69 @@ var CTOS;
             }
         }
         HardDrive.prototype.Init = function () {
+            localStorage.setItem("000", "1@@@001100302ctOS MBR");
+            for (var i = 1; i < 378; ++i) {
+                var tsb = "";
+                if (i < 10) {
+                    tsb += "00" + i.toString();
+                } else if (i >= 10 && i < 100) {
+                    tsb += "0" + i.toString();
+                } else {
+                    tsb += i.toString();
+                }
+                localStorage.setItem(tsb, "0@@@00000000000000000000000000000000000000000000000000000000000");
+            }
+        };
+
+        HardDrive.prototype.SetTSB = function (tsb, data) {
+            if (tsb == "000") {
+                // Should maybe IRQ? Dont write here.
+            } else {
+                localStorage.setItem(tsb, data);
+            }
+        };
+
+        HardDrive.prototype.GetTSB = function (tsb) {
+            return localStorage.getItem(tsb);
+        };
+
+        HardDrive.prototype.SetNextAvailableDir = function (tsb) {
+            var mbr = localStorage.getItem("000");
+            mbr[4] = tsb[0];
+            mbr[5] = tsb[1];
+            mbr[6] = tsb[2];
+            localStorage.setItem("000", mbr);
+        };
+
+        HardDrive.prototype.GetNextAvailableDir = function () {
+            var mbr = localStorage.getItem("000");
+            return mbr.substr(4, 7);
+        };
+
+        HardDrive.prototype.SetNextAvailableData = function (tsb) {
+            var mbr = localStorage.getItem("000");
+            mbr[7] = tsb[0];
+            mbr[8] = tsb[1];
+            mbr[9] = tsb[2];
+            localStorage.setItem("000", mbr);
+        };
+
+        HardDrive.prototype.GetNextAvailableData = function () {
+            var mbr = localStorage.getItem("000");
+            return mbr.substr(7, 10);
+        };
+
+        HardDrive.prototype.SetNextAvailableSwap = function (tsb) {
+            var mbr = localStorage.getItem("000");
+            mbr[10] = tsb[0];
+            mbr[11] = tsb[1];
+            mbr[12] = tsb[2];
+            localStorage.setItem("000", mbr);
+        };
+
+        HardDrive.prototype.GetNextAvailableSwap = function () {
+            var mbr = localStorage.getItem("000");
+            return mbr.substr(10, 13);
         };
 
         HardDrive.prototype.IsSupported = function () {
@@ -31,4 +99,4 @@ var CTOS;
     })();
     CTOS.HardDrive = HardDrive;
 })(CTOS || (CTOS = {}));
-//# sourceMappingURL=harddrive.js.map
+//# sourceMappingURL=hardDrive.js.map
--- source/scripts/host/harddrive.ts
+++ source/scripts/host/harddrive.ts
@@ -2,6 +2,11 @@
     Hard Drive
     Uses HTML5 local storage to access and modify data using Track-Sector-Block.
     The device driver will handle conversions to and from TSB, similar to memoryManager.
+    001-077 = dir data
+    100-377 = data
+    0/1 - @@@ - filename or data
+    in use bit - next TSB or @ invalid - data string from 4 bytes to 63
+    Need 5 TSBs for a single swap file, aprox 15 available from TSB 302
 **/
 
 module CTOS
@@ -10,7 +15,6 @@ module CTOS
     export class HardDrive
     {
         static Supported: boolean = false;
-        private HTML
         
         constructor()
         {
@@ -28,7 +32,86 @@ module CTOS
 
         private Init(): void
         {
+            localStorage.setItem("000", "1@@@001100302ctOS MBR");
+            for (var i: number = 1; i < 378; ++i)
+            {
+                var tsb: string = "";
+                if (i < 10)
+                {
+                    tsb += "00" + i.toString();
+                }
+                else if (i >= 10 && i < 100)
+                {
+                    tsb += "0" + i.toString();
+                }
+                else
+                {
+                    tsb += i.toString();
+                }
+                localStorage.setItem(tsb, "0@@@00000000000000000000000000000000000000000000000000000000000");
+            }
+        }
 
+        public SetTSB(tsb: string, data: string): void
+        {
+            if (tsb == "000")
+            {
+                // Should maybe IRQ? Dont write here.
+            }
+            else
+            {
+                localStorage.setItem(tsb, data);
+            }
+        }
+
+        public GetTSB(tsb: string): string
+        {
+            return localStorage.getItem(tsb);
+        }
+
+        public SetNextAvailableDir(tsb: string): void
+        {
+            var mbr: string = localStorage.getItem("000");
+            mbr[4] = tsb[0];
+            mbr[5] = tsb[1];
+            mbr[6] = tsb[2];
+            localStorage.setItem("000", mbr);
+        }
+
+        public GetNextAvailableDir(): string
+        {
+            var mbr: string = localStorage.getItem("000");
+            return mbr.substr(4, 7);
+        }
+
+        public SetNextAvailableData(tsb: string): void
+        {
+            var mbr: string = localStorage.getItem("000");
+            mbr[7] = tsb[0];
+            mbr[8] = tsb[1];
+            mbr[9] = tsb[2];
+            localStorage.setItem("000", mbr);
+        }
+
+        public GetNextAvailableData(): string
+        {
+            var mbr: string = localStorage.getItem("000");
+            return mbr.substr(7, 10);
+        }
+
+        public SetNextAvailableSwap(tsb: string): void
+        {
+            var mbr: string = localStorage.getItem("000");
+            mbr[10] = tsb[0];
+            mbr[11] = tsb[1];
+            mbr[12] = tsb[2];
+            localStorage.setItem("000", mbr);
+        }
+
+        public GetNextAvailableSwap(): string
+        {
+            var mbr: string = localStorage.getItem("000");
+            return mbr.substr(10, 13);
         }
 
         private IsSupported(): boolean
